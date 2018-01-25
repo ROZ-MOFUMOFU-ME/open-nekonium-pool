776afe66b38ef876d18eec7d8e5eaaf96ab6b50a
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-01-25 15:25:55 +0900

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/rpc"
+	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix

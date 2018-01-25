635477347001e64485f235e933f9ec31cc98f909
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-01-25 15:22:24 +0900

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/rpc"
+	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/storage"
 )
 
 func TestMain(m *testing.M) {

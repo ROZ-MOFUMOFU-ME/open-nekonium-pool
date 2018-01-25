3de92aca4a04f261fb4f121f8e9187a0f8dd7d93
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-01-25 23:27:02 +0900

    Update unlocker.go

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -31,7 +31,7 @@ type UnlockerConfig struct {
 const minDepth = 16
 const byzantiumHardForkHeight = 4370000
 
-var homesteadReward = math.MustParseBig256("5000000000000000000")
+var homesteadReward = math.MustParseBig256("7500000000000000000")
 var byzantiumReward = math.MustParseBig256("3000000000000000000")
 
 // Donate 10% from pool fees to developers

11a7eb8631a782573f76aa85235f2741cefce101
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2019-02-17 06:48:47 +0900

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/ROZ-MOFUMOFU-ME/go-nekonium/common/hexutil"
+	"github.com/nekonium/go-nekonium/common/hexutil"
 
 	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/rpc"
 	"github.com/ROZ-MOFUMOFU-ME/open-nekonium-pool/storage"

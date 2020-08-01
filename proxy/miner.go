c0b21e401db01c4e334f63da95caece49a29e69e
ROZ-MOFUMOFU-ME <mail@mofumofu.me>
2020-08-01 16:19:15 +0900

    fix

--- proxy/miner.go
+++ proxy/miner.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 
 	"github.com/ROZ-MOFUMOFU-ME/nukohash"
-	"github.com/nekonium/go-nekonium/common"
+	"github.com/ROZ-MOFUMOFU-ME/go-nekonium/common"
 )
 
 var hasher = ethash.New()
